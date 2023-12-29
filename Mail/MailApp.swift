21b2d6801e47b393faad9cebc64f51847c31e99d
Nathan Lee <nahtanlee@gmail.com>
2023-12-30 10:14:43 +1100

    Initial Commit

---
+++ Mail/MailApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailApp.swift
+//  Mail
+//
+//  Created by Nathan Lee on 30/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
